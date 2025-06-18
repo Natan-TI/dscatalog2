aedd1cacbba0dcbe12b2acb89e6e9437e0c607f2
Natan-TI <natan201125@hotmail.com>
2025-06-18 01:15:55 -0300

    CategoryDTO

--- backend/src/main/java/com/natanti/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/natanti/dscatalog/services/CategoryService.java
@@ -1,10 +1,12 @@
 package com.natanti.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.natanti.dscatalog.dto.CategoryDTO;
 import com.natanti.dscatalog.entities.Category;
 import com.natanti.dscatalog.repositories.CategoryRepository;
 
@@ -14,7 +16,9 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
-	public List<Category> findAll() {
-		return repository.findAll();
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		List<CategoryDTO> listDTO = list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		return listDTO;
 	}
 }
