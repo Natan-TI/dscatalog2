61d817f5c5256065a7050157fb99d536c7fb8c84
Natan-TI <natan201125@hotmail.com>
2025-06-18 00:25:04 -0300

    CategoryResource

---
+++ backend/src/main/java/com/natanti/dscatalog/services/UserService.java
@@ -0,0 +1,15 @@
+//package com.natanti.dscatalog.services;
+//
+//import com.natanti.dscatalog.entities.User;
+//
+//public class UserService {
+//	
+//	
+//	
+//	public String create() {
+//		User entity = new User();
+//		entity.getPassword();
+//		
+//		return "A";
+//	}
+//}
