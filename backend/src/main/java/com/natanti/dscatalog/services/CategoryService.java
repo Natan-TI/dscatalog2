f0a7d726c50fd49d8828318715c93abeced31ce5
Natan-TI <natan201125@hotmail.com>
2025-06-18 18:24:00 -0300

    Find Category by Id, Exceptions

--- backend/src/main/java/com/natanti/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/natanti/dscatalog/services/CategoryService.java
@@ -1,6 +1,7 @@
 package com.natanti.dscatalog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 import com.natanti.dscatalog.dto.CategoryDTO;
 import com.natanti.dscatalog.entities.Category;
 import com.natanti.dscatalog.repositories.CategoryRepository;
+import com.natanti.dscatalog.services.exceptions.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -20,5 +22,11 @@ public class CategoryService {
 		List<Category> list = repository.findAll();
 		List<CategoryDTO> listDTO = list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 		return listDTO;
+	}
+	
+	public CategoryDTO findById(Long id) {
+		Optional<Category> entity = repository.findById(id);
+		CategoryDTO dto = new CategoryDTO(entity.orElseThrow(() -> new EntityNotFoundException("Categoria não encontrada.")));
+		return dto;
 	}
 }
