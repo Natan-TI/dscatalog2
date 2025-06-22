6c1c62d3546d116be595cd0f54ef6246c7c46792
Natan-TI <natan201125@hotmail.com>
2025-06-22 02:03:11 -0300

    ProductDTO

---
+++ backend/src/main/java/com/natanti/dscatalog/dto/ProductDTO.java
@@ -0,0 +1,54 @@
+package com.natanti.dscatalog.dto;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import com.natanti.dscatalog.entities.Category;
+import com.natanti.dscatalog.entities.Product;
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+public class ProductDTO implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String name;
+	private String description;
+	private Double price;
+	private String imgUrl;
+	private Instant date;
+	
+	private List<CategoryDTO> categories = new ArrayList<>();
+
+	public ProductDTO(Long id, String name, String description, Double price, String imgUrl, Instant date) {
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.imgUrl = imgUrl;
+		this.date = date;
+	}
+	
+	public ProductDTO(Product entity) {
+		this.id = entity.getId();
+		this.name = entity.getName();
+		this.description = entity.getDescription();
+		this.price = entity.getPrice();
+		this.imgUrl = entity.getImgUrl();
+		this.date = entity.getDate();
+	}
+	
+	public ProductDTO(Product entity, Set<Category> categories) {
+		this(entity);
+		categories.forEach(x -> this.categories.add(new CategoryDTO(x)));
+	}
+}
