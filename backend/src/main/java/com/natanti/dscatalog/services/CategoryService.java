0c926dee54b801caff29c119cb6417f039dc7926
Natan-TI <natan201125@hotmail.com>
2025-06-21 22:39:10 -0300

    Auditing data

--- backend/src/main/java/com/natanti/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/natanti/dscatalog/services/CategoryService.java
@@ -5,12 +5,15 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.natanti.dscatalog.dto.CategoryDTO;
 import com.natanti.dscatalog.entities.Category;
 import com.natanti.dscatalog.repositories.CategoryRepository;
+import com.natanti.dscatalog.services.exceptions.DatabaseException;
 import com.natanti.dscatalog.services.exceptions.ResourceNotFoundException;
 
 import jakarta.persistence.EntityNotFoundException;
@@ -53,6 +56,19 @@ public class CategoryService {
 			return new CategoryDTO(existing);
 		} catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id não encontrado: " + id);
+		}
+	}
+	
+	@Transactional(propagation = Propagation.SUPPORTS)
+	public void delete(Long id) {
+		if (!repository.existsById(id)) {
+			throw new ResourceNotFoundException("Id não encontrado: " + id);
+		}
+		
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Falha de integridade referencial!");
 		}
 	}
 	
